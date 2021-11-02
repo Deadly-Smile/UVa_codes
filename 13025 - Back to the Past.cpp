3cbe18632e740c37776f80239852813182c6c585
Anik Saha <aniksaha200r@gmail.com>
2021-11-02 17:05:27 +0600

    Create 13025 - Back to the Past.cpp

---
+++ 13025 - Back to the Past.cpp
@@ -0,0 +1,22 @@
+//----------------------Created by Anik Saha---------------------------//
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO ios_base::sync_with_stdio(false);    cin.tie(NULL)
+#define Ignore cin.ignore()
+typedef unsigned char Byte;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef std::pair<int, int> pairs;
+typedef std::vector<int> VI;
+typedef std::vector<pairs> VII;
+//---------------------------Good luck---------------------------------//
+
+
+int main() {
+    FastIO;
+
+    std::cout << "May 29, 2013 Wednesday" << std::endl;
+    
+    return 0;
+}
