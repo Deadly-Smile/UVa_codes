cc1a86205697bb9cf2004532a7da0356b8facd52
Anik Saha <aniksaha200r@gmail.com>
2022-01-29 12:13:24 +0600

    Create 10469 - To Carry or not to Carry.cpp

---
+++ 10469 - To Carry or not to Carry.cpp
@@ -0,0 +1,25 @@
+//----------------------Created by Anik Saha---------------------------//
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO ios_base::sync_with_stdio(false);    cin.tie(NULL)
+#define Ignore cin.ignore()
+typedef unsigned char Byte;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef std::pair<int, int> pairs;
+typedef std::vector<int> VI;
+typedef std::vector<pairs> VII;
+//---------------------------Good luck---------------------------------//
+
+
+int main() {
+    FastIO;
+
+    ll a{0},b{0};
+    while(std::cin >> a >> b) {
+        std::cout << ((ll)a^b) << std::endl;
+    }
+    
+    return 0;
+}
